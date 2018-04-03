4e4c3eebcc3465d8b5ac3d5312cb9343adacbc16
Yg Versil <ygversil@openmailbox.org>
2018-04-03 12:45:01 +0200

    [cli] Add commands to install and build JS client

--- flexfield/flexfield_cli.py
+++ flexfield/flexfield_cli.py
@@ -5,7 +5,36 @@
 # then you can simply run `flask run`. See <http://flask.pocoo.org/docs/0.12/cli/> for more
 # information.
 
+import os
+import subprocess
+
+import click
+
 from flexfield import create_app, read_config
+import flexfield
 
 config = read_config()
 app = create_app(config)
+
+
+@app.cli.command(name='install-js-deps')
+def install_js_deps():
+    """Run ``npm install`` for the Vue.js client in order to install its JavaScript dependencies."""
+    click.echo('-> Installing JavaScript dependencies for the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(flexfield.__file__),
+                                                              'flexfieldjs')),
+                           'install'])
+    click.echo('-> JavaScript dependencies succesfully installed.')
+
+
+@app.cli.command(name='build-js-client')
+def build_js_client():
+    """Execute ``npm run build`` for the Vue.js client to build it so that it can be served."""
+    click.echo('-> Building the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(flexfield.__file__),
+                                                              'flexfieldjs')),
+                           'run',
+                           'build'])
+    click.echo('-> Vue.js client succesfully built.')
