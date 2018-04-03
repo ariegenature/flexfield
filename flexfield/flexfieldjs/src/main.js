aa4a951de95e2bca426ec848681012eaf005e1bb
Yg Versil <ygversil@openmailbox.org>
2018-04-03 16:24:26 +0200

    [js] Use buefy

--- flexfield/flexfieldjs/src/main.js
+++ flexfield/flexfieldjs/src/main.js
@@ -3,9 +3,13 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
 import router from './router'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
