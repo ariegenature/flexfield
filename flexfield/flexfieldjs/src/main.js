99c310d112928773478a1396b9295ccf180ae437
Yg Versil <ygversil@openmailbox.org>
2018-04-10 17:15:43 +0200

    [js] Add plugin to communicate with an API

--- flexfield/flexfieldjs/src/main.js
+++ flexfield/flexfieldjs/src/main.js
@@ -4,12 +4,14 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import Buefy from 'buefy'
+import VueApi from './plugins/api'
 import router from './router'
 import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
+Vue.use(VueApi)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ flexfield/flexfieldjs/src/plugins/api.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+export default {
+  install (vue) {
+    Vue.prototype.$get = $get
+    Vue.prototype.$post = $post
+  }
+}
+
+export async function $get (url) {
+  try {
+    const response = await axios.get(url)
+    return response
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function $post (url, data, metadata) {
+  try {
+    const response = await axios.post(url, data, metadata)
+    return response
+  } catch (e) {
+    throw e
+  }
+}
