4c497dcdd9e1262ae5b948461339ef1c1cba2e26
Yg Versil <ygversil@openmailbox.org>
2018-04-19 17:32:42 +0200

    [js] Add user object in store
    Update it with data from `/backend/user` API.

--- flexfield/flexfieldjs/src/store/index.js
+++ flexfield/flexfieldjs/src/store/index.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { $get } from '../plugins/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state () {
+    return {
+      user: null
+    }
+  },
+  getters: {
+    user: (state) => state.user
+  },
+  mutations: {
+    user: (state, obj) => {
+      state.user = obj
+    }
+  },
+  actions: {
+    async updateUser ({ commit }) {
+      try {
+        const user = await $get('/backend/user')
+        commit('user', user.data)
+      } catch (e) {
+        console.warn(e)
+        commit('user', null)
+      }
+    }
+  }
 })
