b2c5e40c429a5c08421667fc10c88fafd7e16e0c
Yg Versil <ygversil@openmailbox.org>
2018-04-19 09:53:54 +0200

    Add view decorator to check requested username

--- flexfield/views/backend/__init__.py
+++ flexfield/views/backend/__init__.py
@@ -1,6 +1,27 @@
 """FlexField blueprint to communicate with backend."""
 
-from flask import Blueprint
+from functools import wraps
+
+from flask import Blueprint, abort, current_app, request, redirect, url_for
+from flask_login import current_user
+from flask_login.config import EXEMPT_METHODS as LOGIN_EXEMPT_METHODS
 
 
 backend_bp = Blueprint('backend', __name__, url_prefix='/backend')
+
+
+def same_username_required(func):
+    """Decorator for Flask view functions. The decorated view function must take ``username`` as its
+    first parameter. Then this decorator will ensure that the currently logged in user has the same
+    username (as returned by the ``get_id`` method) than the username passed when calling the view.
+    """
+    @wraps(func)
+    def decorated_view(username, *args, **kwargs):
+        if (request.method in LOGIN_EXEMPT_METHODS or current_app.login_manager._login_disabled):
+            return func(username, *args, **kwargs)
+        if current_user.is_authenticated and current_user.get_id() == username:
+            return func(username, *args, **kwargs)
+        if current_user.is_anonymous:
+            return redirect(url_for('flexfieldjs.login'))
+        return abort(403)
+    return decorated_view
