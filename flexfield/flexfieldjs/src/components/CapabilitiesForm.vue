c5cbe09179185bcb4357d8960ad4f957c53890aa
Yg Versil <ygversil@openmailbox.org>
2018-04-20 15:19:38 +0200

    [js] Add CapabilitiesForm component
    This form allow to choose a study, a protocol and a form for field data.

---
+++ flexfield/flexfieldjs/src/components/CapabilitiesForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form id="capabilities-form" method="POST" accept-charset="UTF-8" v-on:submit.prevent>
+    <form-wizard ref="capabilities-wizard" title="" subtitle="" step-size="xs"
+                 next-button-text="Suivant" back-button-text="Retour"
+                 finish-button-text="Passer au formulaire">
+      <tab-content title="Étude">
+        <div class="content"><p>Protocole</p></div>
+      </tab-content>
+      <tab-content title="Protocole">
+        <div class="content"><p>Protocole</p></div>
+      </tab-content>
+      <tab-content title="Formulaire">
+        <div class="content"><p>Formulaire</p></div>
+      </tab-content>
+    </form-wizard>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CapabilitiesForm'
+}
+</script>
+
+<style>
+@media screen and (max-width: 767px) {
+  #capabilities-form .vue-form-wizard .wizard-nav > li,
+  .wizard-progress-with-circle {
+    display: none
+  }
+  #capabilities-form .vue-form-wizard .wizard-nav > li.active {
+    display: block
+  }
+}
+</style>
