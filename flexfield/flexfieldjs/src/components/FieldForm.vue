24d92c12ba3446d280330a3a7ce4c32d342e9526
Yg Versil <ygversil@openmailbox.org>
2018-04-20 23:52:38 +0200

    [js] Add first version of FieldForm component
    This component will contain form as described by the `currentForm`
    object in store.

---
+++ flexfield/flexfieldjs/src/components/FieldForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form id="field-form" method="POST" accept-charset="UTF-8" v-on:submit.prevent>
+    <form-wizard ref="fieldWizard" title="" subtitle="" step-size="xs"
+                 next-button-text="Suivant" back-button-text="Retour"
+                 finish-button-text="Terminer">
+      <tab-content title="Onglet 1">
+        <div class="content"><p>Onglet 1</p></div>
+      </tab-content>
+      <tab-content title="Onglet 2">
+        <div class="content"><p>Onglet 2</p></div>
+      </tab-content>
+      <tab-content title="Onglet 3">
+        <div class="content"><p>Onglet 3</p></div>
+      </tab-content>
+    </form-wizard>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'FieldForm'
+}
+</script>
+
+<style>
+@media screen and (max-width: 767px) {
+  #field-form .vue-form-wizard .wizard-nav > li,
+  .wizard-progress-with-circle {
+    display: none
+  }
+  #field-form .vue-form-wizard .wizard-nav > li.active {
+    display: block
+  }
+}
+</style>
