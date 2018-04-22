192499e0fab94c15bb2ed590103dd1fe8e71157b
Yg Versil <ygversil@openmailbox.org>
2018-04-22 19:59:34 +0200

    [js] Make FieldForm post new GeoJSON feature on submit
    More exactly, the posted object looks like :
    
    {
      form: <form code>,
      feature: <GeoJSON feature>
    }

--- flexfield/flexfieldjs/src/components/FieldForm.vue
+++ flexfield/flexfieldjs/src/components/FieldForm.vue
@@ -2,7 +2,7 @@
   <form id="field-form" method="POST" accept-charset="UTF-8" v-on:submit.prevent>
     <form-wizard ref="fieldWizard" title="" subtitle="" step-size="xs"
                  next-button-text="Suivant" back-button-text="Retour"
-                 finish-button-text="Terminer">
+                 finish-button-text="Terminer" @on-complete="submitForm">
       <tab-content :title="tab.title" v-for="tab in currentForm.yaml_description.tabs"
                    :item="tab" :key="tab.id">
         <vue-form-generator :model="currentForm.model" :schema="tab.schema"
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'FieldForm',
@@ -26,8 +26,47 @@ export default {
     }
   },
   computed: mapGetters([
-    'currentForm'
-  ])
+    'currentForm',
+    'currentProtocol',
+    'currentStudy',
+    'newFeature'
+  ]),
+  methods: {
+    async submitForm () {
+      this.updateNewFeatureProperties(this.currentForm.model)
+      const additionalProperties = {
+        study: this.currentStudy.code,
+        protocol: this.currentProtocol.code
+      }
+      Object.assign(this.newFeature.properties, additionalProperties)
+      const payload = {
+        form: this.currentForm.code,
+        feature: this.newFeature
+      }
+      try {
+        await this.$post('backend/observation', payload, {
+          headers: {
+            'X-CSRFToken': '«« csrf_token() »»'
+          }
+        })
+        this.$toast.open({
+          duration: 5000,
+          message: 'Votre observation a bien été enregistrée. Merci !',
+          type: 'is-success'
+        })
+      } catch (e) {
+        console.warn(e)
+        this.$toast.open({
+          duration: 5000,
+          message: "Une erreur s'est produite. Veuillez contacter un administrateur.",
+          type: 'is-danger'
+        })
+      }
+    },
+    ...mapActions([
+      'updateNewFeatureProperties'
+    ])
+  }
 }
 </script>
 
