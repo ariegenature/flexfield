5fe83843a5da3366abcf1b8c41c2e56b4c03f683
Yg Versil <ygversil@openmailbox.org>
2018-04-22 13:29:43 +0200

    [js] Init b-input field to use with vue-form-generator

---
+++ flexfield/flexfieldjs/src/components/fieldBInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-field :label="schema.fieldLabel" :type="schema.fieldClass" :message="schema.fieldHelp"
+           :expanded="Boolean(schema.expanded)">
+    <b-input :id="schema.id"
+             :type="schema.inputType ? schema.inputType.toLowerCase() : 'text'"
+             :name="schema.id"
+             v-model="value"
+             :icon="schema.icon"
+             :placeholder="schema.placeholder"
+             :required="schema.required"
+             :autofocus="schema.autofocus"
+             :autocomplete="schema.autocomplete"></b-input>
+  </b-field>
+</template>
+
+<script>
+import { abstractField } from 'vue-form-generator'
+
+export default {
+  name: 'field-b-input',
+  mixins: [abstractField]
+}
+</script>
