efb25f344b0fcdb8ef8df817626792b652f42607
Yg Versil <ygversil@openmailbox.org>
2018-04-23 21:56:25 +0200

    [js] Init b-select field to use with vue-form-generator

---
+++ flexfield/flexfieldjs/src/components/fieldBSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-field :label="schema.fieldLabel" :type="schema.fieldClass" :message="schema.fieldHelp"
+           :expanded="Boolean(schema.expanded)">
+    <b-select :id="schema.id"
+             :name="schema.id"
+             v-model="value"
+             :icon="schema.icon"
+             :required="schema.required"
+             :placeholder="schema.placeholder">
+      <option v-for="option in schema.values" :value="option.id" :key="option.id">
+      {{ option.title }}
+      </option>
+    </b-select>
+  </b-field>
+</template>
+
+<script>
+import { abstractField } from 'vue-form-generator'
+
+export default {
+  name: 'field-b-select',
+  mixins: [abstractField]
+}
+</script>
