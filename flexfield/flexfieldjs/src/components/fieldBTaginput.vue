3661a0a1ec743bdf6578c0ba27bb1e4550dd6d3c
Yg Versil <ygversil@openmailbox.org>
2018-04-24 13:36:14 +0200

    [js] Init b-taginput field to use with vue-form-generator

---
+++ flexfield/flexfieldjs/src/components/fieldBTaginput.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-field :label="schema.fieldLabel" :type="schema.fieldClass" :message="schema.fieldHelp"
+           :expanded="schema.expanded">
+    <b-taginput :id="schema.id"
+                :name="schema.id"
+                v-model="value"
+                :icon="schema.icon"
+                :placeholder="schema.placeholder"
+                :size="schema.size"
+                :type="schema.tagType"
+                :maxlength="schema.tagMaxLength"
+                :maxtags="schema.maxTags"
+                :autofocus="schema.autofocus"
+                :autocomplete="schema.autocomplete"></b-taginput>
+  </b-field>
+</template>
+
+<script>
+import { abstractField } from 'vue-form-generator'
+
+export default {
+  name: 'field-b-taginput',
+  mixins: [abstractField]
+}
+</script>
