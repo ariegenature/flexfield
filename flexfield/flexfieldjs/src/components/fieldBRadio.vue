56631122e33216a1a8e9c91939ec559ecba83bd3
Yg Versil <ygversil@openmailbox.org>
2018-04-24 11:02:23 +0200

    [js] Init b-radio field to use with vue-form-generator

---
+++ flexfield/flexfieldjs/src/components/fieldBRadio.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="columns is-multiline is-centered">
+    <div class="column is-one-third has-text-centered" v-for="option in schema.values"
+         :item="option" :key="option.id">
+      <b-radio href="#"
+               :name="option.id"
+               v-model="value"
+               :size="schema.size"
+               :native-value="option.id">
+        <div v-html="option.text"></div>
+      </b-radio>
+    </div>
+  </div>
+</template>
+
+<script>
+import { abstractField } from 'vue-form-generator'
+
+export default {
+  name: 'field-b-radio',
+  mixins: [abstractField]
+}
+</script>
