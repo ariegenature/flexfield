c22ffb42dbc723b4a11958c2b0ff9b7b7f822c91
Yg Versil <ygversil@openmailbox.org>
2018-04-29 21:06:24 +0200

    [js] Fix field label for radio button
    This is the correct way to have a label.
    
    See https://buefy.github.io/#/documentation/field (section "Combining
    addons and groups")

--- flexfield/flexfieldjs/src/components/fieldBRadioButton.vue
+++ flexfield/flexfieldjs/src/components/fieldBRadioButton.vue
@@ -1,14 +1,17 @@
 <template>
-  <b-field :type="schema.fieldClass" :message="schema.fieldHelp" :expanded="schema.expanded">
-    <b-radio-button href="#" v-for="option in schema.values"
-                    :key="option.id"
-                    :name="option.id"
-                    v-model="value"
-                    :size="schema.size"
-                    :type="schema.fieldType"
-                    :native-value="option.id">
-      <div v-html="option.text"></div>
-    </b-radio-button>
+  <b-field :label="schema.fieldLabel" :type="schema.fieldClass" :message="schema.fieldHelp"
+           :expanded="schema.expanded">
+    <b-field>
+      <b-radio-button href="#" v-for="option in schema.values"
+                      :key="option.id"
+                      :name="schema.id"
+                      v-model="value"
+                      :size="schema.size"
+                      :type="schema.fieldType"
+                      :native-value="option.id">
+        <div v-html="option.text"></div>
+      </b-radio-button>
+    </b-field>
   </b-field>
 </template>
 
