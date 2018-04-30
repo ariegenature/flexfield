e493da46cc020b04b7bab9aaaa5376e05e592c46
Yg Versil <ygversil@openmailbox.org>
2018-04-30 12:21:00 +0200

    [js] Init a GeoJSON layer with observations
    Observations for currently selected form.

--- flexfield/flexfieldjs/src/components/MainMap.vue
+++ flexfield/flexfieldjs/src/components/MainMap.vue
@@ -1,6 +1,8 @@
 <template>
   <l-map ref="map" :zoom="zoom" :center="center" @l-draw-created="emitNewGeometry">
     <l-tile-layer :url="tileURL" :attribution="tileAttrib"></l-tile-layer>
+    <l-geojson ref="observations" :geojson="observations" v-if="observations !== null"
+               :options="observationLayerOptions"></l-geojson>
     <l-geojson ref="new-feature" :geojson="newFeature"
                v-if="newFeature !== null"></l-geojson>
     <leaflet-draw></leaflet-draw>
@@ -9,6 +11,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import L from 'leaflet'
 import LeafletDraw from './LeafletDraw'
 
 export default {
@@ -21,11 +24,25 @@ export default {
       center: [42.857846, 0.626220],
       zoom: 8,
       tileURL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      tileAttrib: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      tileAttrib: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      observationLayerOptions: {
+        pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, {
+            radius: 4,
+            weight: 1,
+            color: '#7A3432',
+            opacity: 1,
+            fillColor: feature.properties.color,
+            fillOpacity: 1,
+            className: 'contribution'
+          })
+        }
+      }
     }
   },
   computed: mapGetters([
-    'newFeature'
+    'newFeature',
+    'observations'
   ]),
   methods: {
     emitNewGeometry (ev) {
