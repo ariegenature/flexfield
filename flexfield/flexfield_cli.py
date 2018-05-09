32dd9892e71d4eeafb18eb642882bb328b354519
Yg Versil <ygversil@openmailbox.org>
2018-05-09 16:50:30 +0200

    Fix wrong example in comment

--- flexfield/flexfield_cli.py
+++ flexfield/flexfield_cli.py
@@ -1,6 +1,6 @@
 # To use this script with Flask, you should assign its path to the FLASK_APP environment variable
 #
-#     > export FLASK_APP=/path/to/flask_cli.py
+#     > export FLASK_APP=/path/to/flexfield_cli.py
 #
 # then you can simply run `flask run`. See <http://flask.pocoo.org/docs/0.12/cli/> for more
 # information.
